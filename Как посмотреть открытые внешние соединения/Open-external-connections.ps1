8c8060e3438bff45725077cdb6b3a99668887bd4
n1rvana <163005667+suhovadm@users.noreply.github.com>
2026-02-15 18:16:53 +0500

    Delete Как посмотреть открытые внешние соединения/Open-external-connections.ps1

--- Как посмотреть открытые внешние соединения/Open-external-connections.ps1
+++
@@ -1,7 +0,0 @@
-Get-NetTCPConnection | 
-Where-Object {$_.State -eq "Established"} |
-Select-Object LocalAddress, LocalPort, RemoteAddress, RemotePort, State |
-Group-Object LocalAddress | ForEach-Object {
-    Write-Host "`nУзел: $($_.Name)" -ForegroundColor Yellow
-    $_.Group | Format-Table -AutoSize
-}
