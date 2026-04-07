34b0386b1af86814d25e165ff4e6893495c61634
n1rvana <163005667+suhovadm@users.noreply.github.com>
2026-04-07 13:44:48 +0500

    Update Get-RAID-Temperature-START_v2.bat

--- _Get-RAID-Temperature/Get-RAID-Temperature-START_v2.bat
+++ _Get-RAID-Temperature/Get-RAID-Temperature-START_v2.bat
@@ -11,7 +11,7 @@ if %errorlevel% neq 0 (
 :: Сворачиваем текущее окно
 powershell -Command "(Get-Process -Id $PID).MainWindowHandle | ForEach-Object {Add-Type '[DllImport(\"user32.dll\")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' -Name Win32ShowWindowAsync -Namespace Win32Functions; [Win32Functions.Win32ShowWindowAsync]::ShowWindowAsync($_, 2)}"
 
-:: Запуск.
+:: Запуск. Если нужно сменить версию при автозагрузке - просто меняем v6 на v5.
 powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Get-RAID-Temperature_v6_utf-8_+enter.ps1"
 
 exit
