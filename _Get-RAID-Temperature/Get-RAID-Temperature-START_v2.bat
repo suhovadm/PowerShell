dd6e66a38377831a39ba72cf993f55085490d3e5
n1rvana <163005667+suhovadm@users.noreply.github.com>
2026-04-07 13:43:38 +0500

    Update Get-RAID-Temperature-START_v2.bat

--- _Get-RAID-Temperature/Get-RAID-Temperature-START_v2.bat
+++ _Get-RAID-Temperature/Get-RAID-Temperature-START_v2.bat
@@ -12,6 +12,6 @@ if %errorlevel% neq 0 (
 powershell -Command "(Get-Process -Id $PID).MainWindowHandle | ForEach-Object {Add-Type '[DllImport(\"user32.dll\")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);' -Name Win32ShowWindowAsync -Namespace Win32Functions; [Win32Functions.Win32ShowWindowAsync]::ShowWindowAsync($_, 2)}"
 
 :: Запуск.
-powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Get-RAID-Temperature_v5_utf-8_+enter.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File "C:\Get-RAID-Temperature_v6_utf-8_+enter.ps1"
 
 exit
